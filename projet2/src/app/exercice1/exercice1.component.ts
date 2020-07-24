c8a63f01aafb8d7d72a48f2acebaff52999ba26e
fredlamie <fred.lamie@hotmail.fr>
2020-07-24 11:22:19 +0200

    projet2

---
+++ projet2/src/app/exercice1/exercice1.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercice1',
+  templateUrl: './exercice1.component.html',
+  styleUrls: ['./exercice1.component.css']
+})
+export class Exercice1Component implements OnInit {
+
+  public exemple1: string = "interpolation de variable";
+  public link: string = "https://dyma.fr";
+  public bgc: string = "red";
+
+  public changeColor(bgc: string): void {
+    this.bgc = bgc;
+  }
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projet2/src/app/exercice2/exercice2.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercice2',
+  templateUrl: './exercice2.component.html',
+  styleUrls: ['./exercice2.component.css']
+})
+export class Exercice2Component implements OnInit {
+  public consoleLogClick(sentence: string) {
+    console.log(sentence);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
